7caf0e8bf22b6c207c30802d2c52c4666f319a8b
MarkusAntonio12 <163982307+MarkusAntonio12@users.noreply.github.com>
2024-05-14 16:03:15 +0200

    cookie_script.js
    A "cookie"-s html oldal JavaScript utasításait tartalmazza

---
+++ cookie_script.js
@@ -0,0 +1,28 @@
+const consentBox =  
+    document.getElementById("consentBox"); 
+const acceptBtn =  
+    document.querySelector(".consentButton"); 
+const rejectBtn =  
+    document.querySelector(".rejectButton"); 
+  
+acceptBtn.onclick = () => { 
+    document.cookie = "CookieBy=GeeksForGeeks; max-age="
+        + 60 * 60 * 24; 
+    if (document.cookie) { 
+        consentBox.classList.add("hide"); 
+    } else { 
+        alert 
+            ("Nem sikerült a művelet! Próbáld újra!"); 
+    } 
+}; 
+  
+rejectBtn.onclick = () => { 
+    alert( 
+        "Cookie-k elutasítva. A funkciók csak korlátozottan állnak rendelkezésre."); 
+    consentBox.classList.add("hide"); 
+}; 
+  
+let checkCookie =  
+    document.cookie.indexOf("CookieBy=GeeksForGeeks"); 
+checkCookie !== -1 ? consentBox.classList.add("hide") : 
+    consentBox.classList.remove("hide");
